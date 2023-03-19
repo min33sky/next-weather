e6feb6e9296a776ce6640b9ff3963dfc06fd6682
min33sky <min33sky@naver.com>
2023-03-19 15:17:57 +0900

    refactor: card css

--- components/WeatherCard.tsx
+++ components/WeatherCard.tsx
@@ -21,14 +21,32 @@ export default function WeatherCard({
   icon,
 }: Props) {
   return (
-    <article className="mt-8 flex flex-col rounded-lg bg-white px-3 py-2 shadow-lg">
-      <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="icon" />
-      <p className="">{description}</p>
-      <p>현재 온도 : {temp}</p>
-      <p>체감 온도 : {feels_like}</p>
-      <p>습도: {humidity}%</p>
-      <p>일출 : {formatTime(sunrise)}</p>
-      <p>일몰 : {formatTime(sunset)}</p>
+    <article className="mt-8 flex w-64 flex-col rounded-lg bg-white px-4 py-2 shadow-2xl">
+      <img
+        className="w-full"
+        src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
+        alt="icon"
+      />
+      <p className="-mt-10 text-center text-3xl font-bold">{temp}℃</p>
+      <p className="mt-2 text-center text-lg font-bold text-brand-500">
+        {description}
+      </p>
+      <p className="mt-2 text-sm">체감 온도 : {feels_like}℃</p>
+      <p className="mt-2 text-sm">습도: {humidity}%</p>
+      <table className="mt-2 border-separate border border-slate-200 text-center">
+        <thead className="">
+          <tr className="bg-slate-200">
+            <th className="">일출</th>
+            <th className="">일몰</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="text-sm">
+            <td>{formatTime(sunrise)}</td>
+            <td>{formatTime(sunset)}</td>
+          </tr>
+        </tbody>
+      </table>
     </article>
   );
 }
